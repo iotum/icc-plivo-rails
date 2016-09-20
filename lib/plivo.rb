b2f26357818f99397716d8a3526ac157692c71e4
Ian Loblaw <ian@iotum.com>
2016-09-20 16:14:17 -0400

    changing all to batch

--- lib/plivo.rb
+++ lib/plivo.rb
@@ -336,7 +336,7 @@ module Plivo
       return request('POST', "/Conference/#{conference_name}/Member/#{member_id}/Mute/", params)
     end
 
-    def mute_all(params={})
+    def mute_batch(params={})
       conference_name = params.delete('conference_name')
       return request('POST', "/Conference/#{conference.name}/Member/Mute/", params)
     end
@@ -347,7 +347,7 @@ module Plivo
       return request('DELETE', "/Conference/#{conference_name}/Member/#{member_id}/Mute/", params)
     end
 
-    def unmute_all(params={})
+    def unmute_batch(params={})
       conference_name = params.delete('conference_name')
       return request('POST', "/Conference/#{conference.name}/Member/Unmute/", params)
     end
